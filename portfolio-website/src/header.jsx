55f430281b752b4ac78a138bb807fe191f2c9b5a
John Lino <jjaviendemonteverde@gmail.com>
2023-10-07 13:16:08 +0800

    set up react with vite and created nav bars

---
+++ portfolio-website/src/header.jsx
@@ -0,0 +1,20 @@
+import './header.css'
+
+export const Header = () => {
+  return (
+    <nav>
+      <a href="" className="nav-bar">
+        Home
+      </a>
+      <a href="" className="nav-bar">
+        About
+      </a>
+      <a href="" className="nav-bar">
+        Projects
+      </a>
+      <a href="" className="nav-bar">
+        Contact
+      </a>
+    </nav>
+  )
+} 
